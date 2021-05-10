87da95e8de9f402f123f81b07577d65475939946
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-05-10 12:46:19 -0300

    Previous problem fixed, working on js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const questionnaire = [{
         "question": "What is the largest ocean in the world?",
-        "answers": ["Atlantic ocean", "Pacific ocean", "Indian ocean"]
+        "answers": ["Atlantic Ocean", "Pacific Ocean", "Indian Ocean"]
     },
     {
         "question": "Who is the best superhero of all time?",
@@ -11,7 +11,7 @@ const questionnaire = [{
     },
     {
         "question": "What is the tallest waterfall in the world?",
-        "answers": ["Niaraga Falls", "The Angel's Jump", "Iguazu Waterfalls"]
+        "answers": ["Niaraga Falls", "The Angels Jump", "Iguazu Waterfalls"]
     },
     {
         "question": "When did World War II end?",
@@ -25,15 +25,18 @@ const questionnaire = [{
 
 //////////////////// Printing on screen ////////////////////
 
+let currentQindex = 0;
 
+let pointsCounter = 0;
+document.querySelector('.points').innerHTML = pointsCounter;
 
 const printQuestion = (i) => {
+    currentQindex++;
     const question = questionnaire[i];
     let answers = question.answers;
-    let rightAnswer = answers[0];
 
 
-    const answersArray = answers.map(currentA => `<p><button onClick="evaluator('${currentA}', this)"> * </button> <span>${currentA}</span></p>`);
+    const answersArray = answers.map(currentA => `<p class="answer"><button onClick="evaluator('${currentA}', this)"> Click </button> <span>${currentA}</span></p>`);
 
     const htmlAnswers = answersArray.join(' ');
 
@@ -43,13 +46,18 @@ const printQuestion = (i) => {
 }
 
 const evaluator = (answer, obj) => {
+    document.querySelectorAll('.answer').forEach(answer => answer.classList.remove('right', 'wrong'));
+
     const parentP = obj.parentNode;
 
-    if (answer == rightAnswer) {
+    if (answer == "Pacific Ocean" || answer == "Deadpool" || answer == "The Angels Jump" || answer == "1945" || answer == "1582") {
         parentP.classList.add('right');
+        pointsCounter = pointsCounter + 100;
+        document.querySelector('.points').innerHTML = pointsCounter;
+        document.getElementById('congrats').innerHTML = "Congratulations!";
     } else {
         parentP.classList.add('wrong');
     }
 }
 
-printQuestion(1);
+setTimeout('printQuestion(currentQindex)', 4000);
